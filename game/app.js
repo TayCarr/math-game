6ce59b3f46492af5b0f8f7c924aeb70ecb82c568
TayCarr <117793653+TayCarr@users.noreply.github.com>
2026-06-19 21:09:07 -0600

    more bones added

--- game/app.js
+++ game/app.js
@@ -21,20 +21,43 @@ const opts = ['*', '/', '+', '-'];
 //max size? of number can ask user, number of questions to ask user
 //ovals is the index num of the op in opts
 //hiddenVal is to set which place is asked, 0->first, 1->second, 2->answer, 3->random
-const game = {maxValue:10, questions:10, oVals:[0, 1, 2, 3], curQue:0, hiddenVal:3};
+const game = {correct:'', maxValue:10, questions:10, oVals:[0, 1, 2, 3], curQue:0, hiddenVal:3, inplay:false};
 
-btn.addEventListener('click', startGame);
-/** 
-for(let i=0; i<100; i++){
-    let val = Math.floor(Math.random() * (game.maxValue + 1))
-    console.log(val);
-}
-*/
+btn.addEventListener('click', btnCheck);
 
-function startGame(){
-    //btn.style.display = 'none'; //make the button disapear once it is clicked
-    game.curQue = 0;
-    buildQuestion();
+answer.addEventListener('keyup', (e)=>{
+    console.log(e.code);
+    console.log(answer.value.length);
+    if(answer.value.length >= 0){
+        btn.style.display = 'block';
+        btn.textContent = 'check';
+        game.inplay = true;
+    }
+    if(e.code == 'Enter'){
+        game.inplay = true;
+        btnCheck();
+    }
+})
+
+function btnCheck(){
+    if(game.inplay){
+        console.log('check');
+        console.log(game.correct);
+        if(answer.value == game.correct){
+            console.log('correct');
+        }
+        else{
+            console.log('wrong, correct is: '+game.correct);
+        }
+        answer.disabled = true;
+        buildQuestion();
+    }
+    else{
+        btn.style.display = 'none'; //make the button disapear once it is clicked
+        game.curQue = 0;
+        buildQuestion();
+    }
+    
 }
 
 function buildQuestion(){
@@ -50,9 +73,14 @@ function buildQuestion(){
             return 0.5 - Math.random(); });//randomize the array
         
         if(game.oVals[0] == 1){
+            //division check
+            //do not want the second value to be 0
+            if(vals[0] == 0){
+                vals[0] = 1;
+            }
+
             let temp = vals[0] * vals[1];
             vals.unshift(temp);
-            console.log(temp);
         }
         else{
             vals[2] = eval(vals[0] + opts[game.oVals[0]] + vals[1]);
@@ -71,11 +99,12 @@ function buildQuestion(){
             hiddenVal = Math.floor(Math.random() * 3);//random location for the hidden val
         }
 
-        console.log(hiddenVal);
         answer.value = '';
+        answer.disabled = false;
 
         for(let i = 0; i < 3; i++){
             if(hiddenVal == i){
+                game.correct = vals[i];
                 output.append(answer);
             }
             else{
@@ -89,7 +118,7 @@ function buildQuestion(){
                 maker('=', 'boxSign');
             }
         }
-
+        answer.focus();
         //vals[hiddenVal] = '__';
         //output.innerHTML = `${vals[0]} ${vals[3]} ${vals[1]} = ${vals[2]}`;
     }  
